3918d5e14f714efc1244ade78e2fb320d5783cf0
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-08 12:36:54 +0500

    made a function for clearing input fields after signup to remove code duplications

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -17,6 +17,11 @@ const SignUpPage = () => {
       email: emailRef.current.value,
       password: passwordRef.current.value,
     };
+    const clearInputFields =()=>{
+      nameRef.current.value = "";
+      emailRef.current.value = "";
+      passwordRef.current.value = "";
+    }
     const { name, email, password } = userSignupData;
     if (!name || !email || !password) {
       toast.error("Please fill all the fields");
@@ -40,18 +45,14 @@ const SignUpPage = () => {
           navigate("/login");
         }, 2000);
         toast.success("signup succesfull redirecting to login page");
-        nameRef.current.value = "";
-        emailRef.current.value = "";
-        passwordRef.current.value = "";
+        clearInputFields() // clearing input fields
       } else if (error) {
         const errorDetails = error?.details[0].message;
         toast.error(errorDetails);
       } else if (!success) {
         toast.error(message);
         console.log(message);
-        nameRef.current.value = "";
-        emailRef.current.value = "";
-        passwordRef.current.value = "";
+        clearInputFields()
 
       }
     } catch (err) {
