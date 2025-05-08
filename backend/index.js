428fad3a7a1c4b29c28d87fb7b6a40e40b0e75df
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-08 16:48:11 +0500

    updated

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./models/db.js";
 import bodyParser from "body-parser";
-import cors from "cors"
+import cors from "cors";
 import authRouter from "./routes/AuthRouter.js";
 import dashboardRouter from "./routes/dashboardRoutes.js";
 
@@ -11,8 +11,8 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(bodyParser.json()) //parse the json data into javascript object
-app.use(cors())
+app.use(bodyParser.json()); //parse the json data into javascript object
+app.use(cors());
 
 // Routes
 app.use("/auth", authRouter);
@@ -20,5 +20,5 @@ app.use("/dashboard", dashboardRouter);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server has started at http://localhost:${PORT}`);
+  console.log(`Server has started at http://localhost:${PORT}`);
 });
