e2c84440215032c3d34cb8901c6a46e2e02f92b5
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-08 16:47:36 +0500

    updated

--- backend/routes/AuthRouter.js
+++ backend/routes/AuthRouter.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { signUpValidation ,loginValidation} from "../middlewares/authValidtion.js";
+import {
+  signUpValidation,
+  loginValidation,
+} from "../middlewares/authValidtion.js";
 import signUp from "../controllers/authController.js";
 import { login } from "../controllers/authController.js";
-const router = express.Router(); 
-router.post("/login", loginValidation,login);
-router.post("/signup", signUpValidation,signUp)
+const router = express.Router();
+router.post("/login", loginValidation, login);
+router.post("/signup", signUpValidation, signUp);
 
-export default router; 
+export default router;
